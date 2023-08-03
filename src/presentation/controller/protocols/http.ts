487070f105b7874de638afd68b8042b70cc23740
mabittar <ma_bittar@yahoo.com.br>
2023-08-03 19:45:30 -0300

    feat: add new errors

---
+++ src/presentation/controller/protocols/http.ts
@@ -0,0 +1,11 @@
+
+export interface HttpRequestProtocol {
+  body?: any
+}
+
+export interface HttpResponseProtocol {
+  statusCode: number,
+  body: any
+}
+
+http://www.nfe.fazenda.gov.br/portal/consultaRecaptcha.aspx?tipoConteudo=XbSeqxE8pl8=&tipoConsulta=completa&nfe=
